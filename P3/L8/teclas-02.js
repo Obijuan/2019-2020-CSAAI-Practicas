752442663b9ad25ec8f69cc19265e13e1b4edbda
Obijuan <juan@iearobotics.com>
2020-03-18 08:06:29 +0100

    P3: Example 2 added

---
+++ P3/L8/teclas-02.js
@@ -0,0 +1,14 @@
+console.log("Ejecutando JS...");
+
+const display = document.getElementById("display");
+
+//-- Tecla pulsada: Mostrar su información
+window.onkeydown = (e)=> {
+  console.log("TEcla pulsada!");
+  display.innerHTML = `Tecla: ${e.key}. Código: ${e.keyCode}`
+}
+
+//-- Tecla liberada: Borrar el párrafo
+window.onkeyup = (e) => {
+  display.innerHTML = ""
+}
